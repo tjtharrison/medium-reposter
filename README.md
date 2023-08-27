fee242ea72b3b818aa084574771614df8794c7af
tjtharrison <gh@tjth.co>
2023-08-27 13:04:05 +0100

    feat: Linkedin authentication working

--- README.md
+++ README.md
@@ -12,3 +12,6 @@ To run the project, run the following command:
 python main.py
 ```
 
+```commandline
+curl -d "grant_type=authorization_code&code=AQQwrKmFbRQVfFMDxJUnWFSFrqJLOu9Hfv_8iwQxEXhPArRYcemOG8VGsRDFTiE7jw8KHPaIbvMbWHDLJ9GiA9WRNWRHY7&redirect_uri=https://readme.tjth.co/auth/linkedin/callback&client_id=78z23elllucg62&client_secret=qjYXM3sAuspomBu9" -H "application/x-www-form-urlencoded" -X POST "www.linkedin.com/oauth/v2/accessToken"
+```
--- bin/linkedin.py
+++ bin/linkedin.py
@@ -1,55 +1,110 @@
-"""Module to handle LinkedIn API calls."""
-
-import json
-import logging
-import requests
 import os
+import requests
 
+from dotenv import load_dotenv
+load_dotenv()
 
-def get_user_id(access_token):
+headers = {
+    'Authorization': 'Bearer ' + os.environ.get("LINKEDIN_AUTH_TOKEN"),
+    'Connection': 'Keep-Alive',
+    'Content-Type': 'application/x-www-form-urlencoded',
+}
+
+data_auth = {
+    'token': os.environ.get("LINKEDIN_AUTH_TOKEN"),
+    'client_id': os.environ.get("LINKEDIN_CLIENT_ID"),
+    'client_secret': os.environ.get("LINKEDIN_CLIENT_SECRET"),
+}
+
+def validate_token():
     """
-    Get the user ID from LinkedIn.
-
-    Args:
-        access_token: The access token to use.
+    Validate the provided token.
 
     Returns:
-        The user ID.
+        True if the token is valid, False otherwise.
     """
-
-
-def create_article(post_detail, access_token):
-    """Create an article.
-
-    Args:
-        post_detail: The post detail to create the article from.
-        access_token: The access token to use.
-
-    Returns:
-        True if successful
-    """
-    logging.info("Creating Linkedin article for %s", post_detail["title"])
-
-    headers = {
-        "X-Restli-Protocol-Version": "2.0.0",
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {access_token}",
-    }
-
-    # Create the article
-    article = {
-        "author": f"urn:li:person:{get_user_id(access_token)}",
-        "lifecycleState": "DRAFT",
-        "visibility": "PUBLIC",
-        "content": {"contentEntities": [{"entityLocation": post_detail["link"]}]},
-    }
 
     response = requests.post(
-        "https://api.linkedin.com/v2/ugcPosts",
+        'https://www.linkedin.com/oauth/v2/introspectToken',
         headers=headers,
-        data=json.dumps(article),
+        data=data_auth
+    )
+
+    if response.status_code == 200 and response.json()['active']:
+        return True
+
+def get_user_info():
+    """
+    Get the user info.
+
+    Returns:
+        The user info.
+    """
+
+    response = requests.get(
+        'https://api.linkedin.com/v2/me',
+        headers=headers,
+        data=data_auth
     )
 
     print(response.text)
 
-    return True
+    if response.status_code == 200:
+        return response.json()
+
+# def post_article():
+#     """
+#     Post an article on LinkedIn.
+#
+#     Returns:
+#         True if the article was posted successfully, False otherwise.
+#     """
+#     response = requests.post(
+#         'https://api.linkedin.com/v2/ugcPosts',
+#         headers=headers,
+#         data=data
+#     )
+#
+#     # {
+#     #     'author': 'urn:li:person:<your_linkedin_id>',
+#     #     'lifecycleState': 'PUBLISHED',
+#     #     'specificContent': {
+#     #         'com.linkedin.ugc.ShareContent': {
+#     #             'shareCommentary': {
+#     #                 'text': 'Check out our latest blog post!',
+#     #             },
+#     #             'shareMediaCategory': 'ARTICLE',
+#     #             'media': [
+#     #                 {
+#     #                     'status': 'READY',
+#     #                     'description': {
+#     #                         'text': 'Read our latest blog post about LinkedIn API!',
+#     #                     },
+#     #                     'originalUrl': '<your_blog_post_url>',
+#     #                 },
+#     #             ],
+#     #         },
+#     #     },
+#     #     'visibility': {
+#     #         'com.linkedin.ugc.MemberNetworkVisibility': 'PUBLIC',
+#     #     },
+#     # }
+#
+#     print(response.text)
+#
+#     if response.status_code == 201:
+#         return True
+
+def main():
+    """Main function."""
+
+    if validate_token():
+        print('Token is valid')
+
+        user_info = get_user_info()
+        print(user_info)
+
+
+
+if __name__ == '__main__':
+    main()
